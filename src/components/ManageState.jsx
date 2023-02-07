0862cbe9ae531e351a148767dba61ddc03bdc773
yurigligoric <yurigligoric@gmail.com>
2023-02-07 13:55:07 +0800

    update

---
+++ src/components/ManageState.jsx
@@ -0,0 +1,25 @@
+
+
+import React from 'react'
+
+const ManageState = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ManageState
+
+
+
+//** CLICK PARAGRAPH */
+// let para = document.getElementsByTagName("p");
+
+// [...para].forEach(elem => elem.addEventListener('click', readValue));
+
+// function readValue(e) {
+//   let text = e.target.textContent;
+//   console.log(text);
+// }
